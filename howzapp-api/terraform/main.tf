745dc7d992c9aa0d617246864f83f5d9e553c3c2
Irsath Kareem <mohdirsath1997@gmail.com>
2025-11-11 08:43:18 +0530

    debug deployment done (seventeenth revision)

--- howzapp-api/terraform/main.tf
+++ howzapp-api/terraform/main.tf
@@ -32,7 +32,7 @@ variable machine_type {
 
 variable machine_image {
   type = string
-  default = "projects/debian-cloud/global/images/debian-12-bookworm-arm64-v20251014"
+  default = "projects/debian-cloud/global/images/debian-12-bookworm-v20251014"
 }
 
 variable "disk_type" {
@@ -118,6 +118,69 @@ resource "google_compute_instance" "app-server" {
     enable_vtpm                 = true
   }
 }
+
+# resource "google_compute_instance" "instance-20251111-004649" {
+#   boot_disk {
+#     auto_delete = true
+#     device_name = "instance-20251111-004649"
+#
+#     initialize_params {
+#       image = "projects/debian-cloud/global/images/debian-12-bookworm-v20251014"
+#       size  = 10
+#       type  = "pd-standard"
+#     }
+#
+#     mode = "READ_WRITE"
+#   }
+#
+#   can_ip_forward      = false
+#   deletion_protection = false
+#   enable_display      = false
+#
+#   labels = {
+#     goog-ec-src = "vm_add-tf"
+#   }
+#
+#   machine_type = "e2-micro"
+#
+#   metadata = {
+#     startup-script = "kajsdh\nakjsdhakjdh\naslkdjalksd\naskldjalksdhl"
+#   }
+#
+#   name = "instance-20251111-004649"
+#
+#   network_interface {
+#     access_config {
+#       network_tier = "STANDARD"
+#     }
+#
+#     queue_count = 0
+#     stack_type  = "IPV4_ONLY"
+#     subnetwork  = "projects/howzapp-techullurgy-full/regions/us-central1/subnetworks/default"
+#   }
+#
+#   scheduling {
+#     automatic_restart   = false
+#     on_host_maintenance = "TERMINATE"
+#     preemptible         = true
+#     provisioning_model  = "SPOT"
+#   }
+#
+#   tags = ["app-ports"]
+#
+#   service_account {
+#     email  = "620351186909-compute@developer.gserviceaccount.com"
+#     scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+#   }
+#
+#   shielded_instance_config {
+#     enable_integrity_monitoring = true
+#     enable_secure_boot          = false
+#     enable_vtpm                 = true
+#   }
+#
+#   zone = "us-central1-f"
+# }
 
 # resource "google_compute_instance" "instance-20251111-004649" {
 #   boot_disk {
