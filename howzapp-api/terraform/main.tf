f6573973b8b0a3f553ef9dda627d4b06ca498c42
Irsath Kareem <mohdirsath1997@gmail.com>
2025-11-11 06:54:59 +0530

    debug deployment done (thirteenth revision)

--- howzapp-api/terraform/main.tf
+++ howzapp-api/terraform/main.tf
@@ -59,51 +59,57 @@ resource "google_compute_firewall" "allow_app_ports" {
   target_tags = ["app-ports"]
 }
 
-resource "google_compute_instance" "app_server" {
-  name = "app-server"
-  machine_type = var.machine_type
-
-  zone = var.zone
-
-  metadata = {
-    # ssh-keys = sensitive("${var.ssh_user}:${file("~/.ssh/id_rsa.pub")}")
-
-    startup-script = file("${path.root}/scripts/main.sh")
-  }
-
-  boot_disk {
-    device_name = "app-server"
-
-    initialize_params {
-      image = var.machine_image
-      size = var.disk_size
-      type = var.disk_type
-    }
-  }
-
-  network_interface {
-    access_config {
-      network_tier = "STANDARD"
-    }
-    queue_count = 0
-    stack_type  = "IPV4_ONLY"
-    subnetwork  = "projects/howzapp-techullurgy-full/regions/us-central1/subnetworks/default"
-  }
-
-  tags = ["app-ports"]
-
-  scheduling {
-    automatic_restart   = false
-    on_host_maintenance = "TERMINATE"
-    preemptible         = true
-    provisioning_model  = "SPOT"
-  }
-
-  service_account {
-    email  = "620351186909-compute@developer.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
-  }
-}
+# resource "google_compute_instance" "app_server" {
+#   name = "app-server"
+#   machine_type = var.machine_type
+#
+#   zone = var.zone
+#
+#   metadata = {
+#     # ssh-keys = sensitive("${var.ssh_user}:${file("~/.ssh/id_rsa.pub")}")
+#
+#     startup-script = file("${path.root}/scripts/main.sh")
+#   }
+#
+#   boot_disk {
+#     device_name = "app-server"
+#
+#     initialize_params {
+#       image = var.machine_image
+#       size = var.disk_size
+#       type = var.disk_type
+#     }
+#   }
+#
+#   network_interface {
+#     access_config {
+#       network_tier = "STANDARD"
+#     }
+#     queue_count = 0
+#     stack_type  = "IPV4_ONLY"
+#     subnetwork  = "projects/howzapp-techullurgy-full/regions/us-central1/subnetworks/default"
+#   }
+#
+#   tags = ["app-ports"]
+#
+#   scheduling {
+#     automatic_restart   = false
+#     on_host_maintenance = "TERMINATE"
+#     preemptible         = true
+#     provisioning_model  = "SPOT"
+#   }
+#
+#   service_account {
+#     email  = "620351186909-compute@developer.gserviceaccount.com"
+#     scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+#   }
+#
+#   shielded_instance_config {
+#     enable_integrity_monitoring = true
+#     enable_secure_boot          = false
+#     enable_vtpm                 = true
+#   }
+# }
 
 resource "google_compute_instance" "instance-20251111-004649" {
   boot_disk {
@@ -151,6 +157,8 @@ resource "google_compute_instance" "instance-20251111-004649" {
     preemptible         = true
     provisioning_model  = "SPOT"
   }
+
+  tags = ["app-ports"]
 
   service_account {
     email  = "620351186909-compute@developer.gserviceaccount.com"
