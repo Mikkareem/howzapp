35fc7631d503e55935ce8706fcb6780b62b7891e
Irsath Kareem <mohdirsath1997@gmail.com>
2025-11-17 17:32:17 +0530

    :composeApp build.gradle.kts deps optimizations

--- howzapp-app/composeApp/build.gradle.kts
+++ howzapp-app/composeApp/build.gradle.kts
@@ -13,27 +13,16 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
-            implementation(libs.androidx.lifecycle.process)
             implementation(libs.koin.android)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
-            implementation(libs.androidx.lifecycle.viewmodelCompose)
-            implementation(libs.androidx.lifecycle.runtimeCompose)
 
             implementation(libs.androidx.navigation3.runtime)
             implementation(libs.androidx.navigation3.ui)
             implementation(libs.lifecycle.viewmodel.navigation3)
-
-            implementation(project.dependencies.platform(libs.koin.bom))
-            implementation(libs.koin.core)
-            implementation(libs.koin.compose)
-            implementation(libs.koin.compose.viewmodel)
 
             implementation(projects.core.di)
             implementation(projects.core.designsystem)
